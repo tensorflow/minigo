5f6ed0d64e222b50d0269712cda384d31c08c60c
jacksona <jacksona@google.com>
2018-01-04 16:08:18 -0800

    add the right flags to make grep break on first match.  makes it much faster.

--- modelstats.sh
+++ modelstats.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-for i in `ls $1 | tail`;
+for i in `ls $1 | tail -n 5`;
   do
   echo $i
   find sgf19/$i/ -name "*.sgf" | wc -l;
   echo -en "B+\t\t"
-  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep "B+" | wc -l
+  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep -m 1 "B+" | wc -l
   echo -en "W+\t\t"
-  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep "W+" | wc -l
+  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep -m 1 "W+" | wc -l
   echo -en "B+Resign\t"
-  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep "B+R" | wc -l
+  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep -m 1 "B+R" | wc -l
   echo -en "W+Resign\t"
-  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep "W+R" | wc -l
+  find sgf19/$i/ -name "*.sgf" -print0 | xargs -0 grep -m 1 "W+R" | wc -l
   #echo "Stats:"
   #find sgf19/$i/ -name "*.sgf" -exec /bin/sh -c 'tr -cd \; < {} | wc -c' \; | ministat -n 
   echo -en "\n"
