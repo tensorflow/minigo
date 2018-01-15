fbf95467ec68e6860d93dea22c2d6ab8588e041b
Josh Hoak <jrhoak@gmail.com>
2018-01-15 15:14:03 -0800

    Pull summary of the coordinates into a table.

--- coords.py
+++ coords.py
@@ -2,12 +2,21 @@
 
 This introduces some helpers and terminology that are used throughout MiniGo.
 
-Coord/Coordinate: This is a tuple of the form (column, row) that's indexed from
-    the upper-left.
+MiniGo Coord/Coordinate: This is a tuple of the form (column, row) that's
+    indexed from (0,0) from the upper-left.
 Flattened Coordinate: this is a number ranging from 0 - N^2 (so N^2+1
     possible values). The extra value N^2 is used to mark a 'pass' move.
 KGS Coordinate: Human-readable coordinate indexed from bottom left.
 GTP Coordinate: Tuple coordinate indexed starting at 1,1 from top-left (r, c)
+
+So, for a 19x19,
+
+Coord Type      upper_left      upper_right     pass
+-------------------------------------------------------
+minigo coord    (0, 0)          (18, 0)         None
+flat            0               18              361
+KGS             'A19'           'T19'           'pass'
+GTP             (1, 1)          (1,19)          'pass'
 """
 
 import go
@@ -25,27 +34,13 @@ def kgs_to_flat(sgf):
     return flatten_coords(parse_kgs_coords(sgf))
 
 def flatten_coords(c):
-    """Flattens a coordinate tuple.
-
-    Takes coordinates of the form (col, row) indexed from the upper-Left and
-    returns a number between 0 and go-board-size^2-1 (i.e., N^2-1), unless the
-    row/column is unspecified, in which case, N^2 is returned.
-
-    Note here that None coordinates represent pass moves, so N^2 is a
-    pass-move.
-    """
+    """Flattens a coordinate tuple from a MiniGo Coordinate"""
     if c is None:
         return go.N * go.N
     return go.N * c[0] + c[1]
 
 def unflatten_coords(f):
-    """Unflattens a flattened coordinate into a tuple.
-
-    Takes a flattened coordinate (integer) and returns a coordinate tuple.
-
-    Note that a flattened coordinate of N^2 becomes None, to represent
-    pass-moves.
-    """
+    """Unflattens a flattened coordinate into a Minigo Coordinate"""
     if f == go.N * go.N:
         return None
     return divmod(f, go.N)
@@ -69,11 +64,7 @@ def unparse_sgf_coords(c):
     return SGF_COLUMNS[c[1]] + SGF_COLUMNS[c[0]]
 
 def parse_kgs_coords(s):
-    """Interprets KGS coordinates returning a coordinate tuple (c, r).
-
-    In KGS terminology, A1 is bottom left; A19 is top left. So, to transform to
-    a standard coordinate tuple (c,r), we need to know the board size.
-    """
+    """Interprets KGS coordinates returning a coordinate tuple (c, r)."""
     if s == 'pass':
         return None
     s = s.upper()
@@ -82,7 +73,7 @@ def parse_kgs_coords(s):
     return go.N - row_from_bottom - 1, col
 
 def to_human_coord(coord):
-    """Converts from a MiniGo coord to a human readable string
+    """Converts from a MiniGo coord to a human readable string.
 
     This is equivalent to a KGS coordinate.
     """
@@ -95,8 +86,7 @@ def to_human_coord(coord):
 def parse_pygtp_coords(vertex):
     """Transforms a GTP coordinate into a standard MiniGo coordinate (c, r)
 
-    GTP coordinates are interpeted as (1, 1) being the bottom left and (1, 19)
-    the top left. GTP has a notion of both a Pass and a Resign, both of which
+    GTP has a notion of both a Pass and a Resign, both of which
     are mapped to None so the conversion is not precisely bijective.
     """
     if vertex in (gtp.PASS, gtp.RESIGN):
@@ -104,10 +94,7 @@ def parse_pygtp_coords(vertex):
     return go.N - vertex[1], vertex[0] - 1
 
 def unparse_pygtp_coords(c):
-    """Transforms a MiniGo Coordinate back into a GTP coordinate
-
-    As in other coordinates, None is interpreted as a Pass.
-    """
+    """Transforms a MiniGo Coordinate back into a GTP coordinate."""
     if c is None:
         return gtp.PASS
     return c[1] + 1, go.N - c[0]
