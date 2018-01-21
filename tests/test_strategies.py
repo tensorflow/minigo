df4a44375f2f2dcfa51fc1fd20c3b223cb78a64c
Brian Lee <brian.kihoon.lee@gmail.com>
2018-01-21 16:17:13 -0500

    Parallel tree search working

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -47,14 +47,26 @@ class DummyNet():
     def run_many(self, positions):
         return [self.fake_priors] * len(positions), [self.fake_value] * len(positions)
 
+def initialize_basic_player():
+    player = MCTSPlayerMixin(DummyNet())
+    player.initialize_game()
+    first_node = player.root.select_leaf()
+    first_node.incorporate_results(
+        *player.network.run(player.root.position), up_to=player.root)
+    return player
+
+def initialize_almost_done_player():
+    probs = np.array([.001] * (go.N * go.N + 1))
+    probs[2:5] = 0.2 # some legal moves along the top.
+    probs[-1] = 0.2 # passing is also ok
+    net = DummyNet(fake_priors=probs)
+    player = MCTSPlayerMixin(net)
+    # root position is white to play with no history == white passed.
+    player.initialize_game(SEND_TWO_RETURN_ONE)
+    return player
+
+
 class TestMCTSPlayerMixin(GoPositionTestCase, MCTSTestMixin):
-    def setUp(self):
-        self.player = MCTSPlayerMixin(DummyNet())
-        self.player.initialize_game()
-        first_node = self.player.root.select_leaf()
-        first_node.add_virtual_loss(self.player.root)
-        first_node.incorporate_results(
-            *self.player.network.run(self.player.root.position), up_to=self.player.root)
 
     def test_time_controls(self):
         secs_per_move = 5
@@ -70,7 +82,7 @@ class TestMCTSPlayerMixin(GoPositionTestCase, MCTSTestMixin):
             self.assertGreater(total_time_spent, time_limit * 0.95)
 
     def test_inject_noise(self):
-        player = self.player
+        player = initialize_basic_player()
         sum_priors = np.sum(player.root.prior)
         self.assertAlmostEqual(sum_priors, 1) # dummyNet should return normalized priors.
         self.assertTrue(np.all(player.root.child_U == player.root.child_U[0]))
@@ -85,7 +97,8 @@ class TestMCTSPlayerMixin(GoPositionTestCase, MCTSTestMixin):
         self.assertGreater(max_p, 3/(go.N ** 2 + 1))
 
     def test_pick_moves(self):
-        root = self.player.root
+        player = initialize_basic_player()
+        root = player.root
         root.child_N[coords.flatten_coords((2, 0))] = 10
         root.child_N[coords.flatten_coords((1, 0))] = 5
         root.child_N[coords.flatten_coords((3, 0))] = 1
@@ -94,40 +107,32 @@ class TestMCTSPlayerMixin(GoPositionTestCase, MCTSTestMixin):
 
 
         # Assert we're picking deterministically
-        self.assertTrue(root.position.n > self.player.temp_threshold)
-        move = self.player.pick_move()
+        self.assertTrue(root.position.n > player.temp_threshold)
+        move = player.pick_move()
         self.assertEqual(move, (2, 0))
 
         # But if we're in the early part of the game, pick randomly
         root.position.n = 3
-        self.assertFalse(self.player.root.position.n > self.player.temp_threshold)
+        self.assertFalse(player.root.position.n > player.temp_threshold)
 
         with mock.patch('random.random', lambda: .5):
-            move = self.player.pick_move()
+            move = player.pick_move()
             self.assertEqual(move, (2, 0))
 
         with mock.patch('random.random', lambda: .99):
-            move = self.player.pick_move()
+            move = player.pick_move()
             self.assertEqual(move, (3,0))
 
     def test_dont_pass_if_losing(self):
-        # force exploration of our known set of moves by constructing the
-        # probabilities passed in when expanding nodes.
-        probs = np.array([.001] * (go.N * go.N + 1))
-        probs[2:5] = 0.2 # some legal moves along the top.
-        probs[-1] = 0.2 # passing is also ok
-        net = DummyNet(fake_priors=probs)
-        player = MCTSPlayerMixin(net)
-        # root position is white to play with no history == white passed.
-        player.initialize_game(SEND_TWO_RETURN_ONE)
+        player = initialize_almost_done_player()
 
         # check -- white is losing.
         self.assertEqual(player.root.position.score(), -0.5)
 
         for i in range(20):
             player.tree_search()
-            # uncomment to debug this test
-            # print(player.root.describe())
+        # uncomment to debug this test
+        # print(player.root.describe())
 
         # Search should converge on D9 as only winning move.
         best_move = np.argmax(player.root.child_N)
@@ -142,3 +147,24 @@ class TestMCTSPlayerMixin(GoPositionTestCase, MCTSTestMixin):
         # uncomment to debug this test
         # print(player.root.describe())
 
+    def test_parallel_tree_search(self):
+        player = initialize_almost_done_player()
+        # check -- white is losing.
+        self.assertEqual(player.root.position.score(), -0.5)
+        # virtual losses should enable multiple searches to happen simultaneously
+        # without throwing an error...
+        for i in range(5):
+            player.tree_search(num_parallel=8)
+        # uncomment to debug this test
+        # print(player.root.describe())
+
+        # Search should converge on D9 as only winning move.
+        best_move = np.argmax(player.root.child_N)
+        self.assertEqual(best_move, kgs_to_flat('D9'))
+        # D9 should have a positive value
+        self.assertGreater(player.root.children[kgs_to_flat('D9')].Q, 0)
+        self.assertGreaterEqual(player.root.N, 20)
+        # passing should be ineffective.
+        self.assertLess(player.root.child_Q[-1], 0)
+        # no virtual losses should be pending
+        self.assertNoPendingVirtualLosses(player.root)
