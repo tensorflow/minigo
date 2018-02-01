52abf98a498829cddcdfc11ef9fe9ba55525630b
Josh Hoak <jrhoak@gmail.com>
2018-02-01 15:19:31 -0800

    Several python cleanups.
    - Add some __init__.py files so we can make importing work.
    - Move shipnames into shipname.py. (Not having to drag the data.txt around is nice).
    - Add a basic .pylintrc file, generated with `pylint --generate-rcfile > .pylintrc`
    - Ignore all files except cloud_logging and coords, which I've fixed up.
      I'll fix up the other files in a later CL.
    - Add a test.sh script for running linting + tests.

---
+++ test.sh
@@ -0,0 +1,22 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+#
+# Simple shell script to lint files and run the tests. Could be helpful for
+# users, but largely for automation.
+
+ls *.py | xargs pylint
+
+BOARD_SIZE=9 python3 -m unittest discover tests
