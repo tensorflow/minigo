9664cfce9a1a13d3519299532961044910536ac1
jacksona <jacksona@google.com>
2018-03-21 23:09:09 -0700

    add convert command to main.py

--- main.py
+++ main.py
@@ -77,8 +77,8 @@ def gtp(load_file: "The path to the network model files"=None,
 
 
 def bootstrap(
-    working_dir: 'tf.estimator working directory.',
-    model_save_path: 'Where to export the first bootstrapped generation'):
+        working_dir: 'tf.estimator working directory.',
+        model_save_path: 'Where to export the first bootstrapped generation'):
     _ensure_dir_exists(working_dir)
     _ensure_dir_exists(os.path.dirname(model_save_path))
     dual_net.bootstrap(working_dir)
@@ -86,10 +86,10 @@ def bootstrap(
 
 
 def train(
-    working_dir: 'tf.estimator working directory.',
-    chunk_dir: 'Directory where gathered training chunks are.',
-    model_save_path: 'Where to export the completed generation.',
-    generation_num: 'Which generation you are training.'=0):
+        working_dir: 'tf.estimator working directory.',
+        chunk_dir: 'Directory where gathered training chunks are.',
+        model_save_path: 'Where to export the completed generation.',
+        generation_num: 'Which generation you are training.'=0):
     tf_records = sorted(gfile.Glob(os.path.join(chunk_dir, '*.tfrecord.zz')))
     tf_records = tf_records[-1 * (WINDOW_SIZE // EXAMPLES_PER_RECORD):]
 
@@ -101,10 +101,10 @@ def train(
 
 
 def validate(
-    working_dir: 'tf.estimator working directory',
-    *tf_record_dirs: 'Directories where holdout data are',
-    checkpoint_name: 'Which checkpoint to evaluate (None=latest)'=None,
-    validate_name: 'Name for validation set (i.e. selfplay or human)'=None):
+        working_dir: 'tf.estimator working directory',
+        *tf_record_dirs: 'Directories where holdout data are',
+        checkpoint_name: 'Which checkpoint to evaluate (None=latest)'=None,
+        validate_name: 'Name for validation set (i.e. selfplay or human)'=None):
     tf_records = []
     with timer("Building lists of holdout files"):
         for record_dir in tf_record_dirs:
@@ -113,7 +113,7 @@ def validate(
     with timer("Validating from {} to {}".format(os.path.basename(tf_records[0]),
                                                  os.path.basename(tf_records[-1]))):
         dual_net.validate(working_dir, tf_records, checkpoint_name=checkpoint_name,
-            name=validate_name)
+                          name=validate_name)
 
 
 def evaluate(
@@ -219,9 +219,40 @@ def gather(
         f.write('\n'.join(sorted(already_processed)))
 
 
+def convert(load_file, dest_file):
+    from tensorflow.python.framework import meta_graph
+    features, labels = dual_net.get_inference_input()
+    dual_net.model_fn(features, labels, tf.estimator.ModeKeys.PREDICT,
+                      dual_net.get_default_hyperparams())
+    sess = tf.Session()
+
+    # retrieve the global step as a python value
+    ckpt = tf.train.load_checkpoint(load_file)
+    global_step_value = ckpt.get_tensor('global_step')
+
+    # restore all saved weights, except global_step
+    meta_graph_def = meta_graph.read_meta_graph_file(
+        load_file + '.meta')
+    stored_var_names = set([n.name
+                            for n in meta_graph_def.graph_def.node
+                            if n.op == 'VariableV2'])
+    stored_var_names.remove('global_step')
+    var_list = [v for v in tf.global_variables()
+                if v.op.name in stored_var_names]
+    tf.train.Saver(var_list=var_list).restore(sess, load_file)
+
+    # manually set the global step
+    global_step_tensor = tf.train.get_or_create_global_step()
+    assign_op = tf.assign(global_step_tensor, global_step_value)
+    sess.run(assign_op)
+
+    # export a new savedmodel that has the right global step type
+    tf.train.Saver().save(sess, dest_file)
+
+
 parser = argparse.ArgumentParser()
 argh.add_commands(parser, [gtp, bootstrap, train,
-                           selfplay, gather, evaluate, validate])
+                           selfplay, gather, evaluate, validate, convert])
 
 if __name__ == '__main__':
     cloud_logging.configure()
