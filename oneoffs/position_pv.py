2dec146cf51e5912328b107c4f8546ee0300ad1d
Seth Troisi <braintwo@gmail.com>
2018-04-18 14:17:33 -0700

    new oneoff script to constructing PVs of a set of positions (#157)
    * new changes
    
    * position_pv
    
    * revert training_curve
    
    * this is getting squashed anyways

--- oneoffs/position_pv.py
+++ oneoffs/position_pv.py
@@ -1,9 +1,9 @@
 """
-Used to plot a heatmap of the policy and value networks.
+Used to find PV for position.
 Check FLAGS for default values.
 
 Usage:
-python heatmap.py
+python position_pv.py
 
 """
 import sys
@@ -17,11 +17,12 @@ import tensorflow as tf
 from tqdm import tqdm
 
 import go
+import strategies
 import oneoff_utils
 
 
 tf.app.flags.DEFINE_string(
-    "sgf_dir", "sgf/baduk_db/", "Path to directory of sgf to analyze")
+    "sgf_dir", "sgf/baduk_db/", "sgf database")
 
 tf.app.flags.DEFINE_string("model_dir", "saved_models",
                            "Where the model files are saved")
@@ -30,20 +31,13 @@ tf.app.flags.DEFINE_integer("idx_start", 150,
                             "Only take models after given idx")
 tf.app.flags.DEFINE_integer("eval_every", 5,
                             "Eval every k models to generate the curve")
+tf.app.flags.DEFINE_integer("readouts", 1000,
+                            "Eval every k models to generate the curve")
 
 FLAGS = tf.app.flags.FLAGS
 
 
-def eval_policy(eval_positions)
-    """Evaluate all positions with all models save the policy heatmaps as CSVs
-
-    CSV name is "heatmap-<position_name>-<model-index>.csv"
-    CSV format is: model number, value network output, policy network outputs
-
-    position_name is taken from the SGF file
-    Policy network outputs (19x19) are saved in flat order (see coord.from_flat)
-    """
-
+def eval_pv(eval_positions):
     model_paths = oneoff_utils.get_model_paths(FLAGS.model_dir)
 
     idx_start = FLAGS.idx_start
@@ -51,46 +45,51 @@ def eval_policy(eval_positions)
 
     print("Evaluating models {}-{}, eval_every={}".format(
           idx_start, len(model_paths), eval_every))
-
     for idx in tqdm(range(idx_start, len(model_paths), eval_every)):
         if idx == idx_start:
             player = oneoff_utils.load_player(model_paths[idx])
         else:
             oneoff_utils.restore_params(model_paths[idx], player)
 
-        pos_names, positions = zip(*eval_positions)
-        # This should be batched at somepoint.
-        eval_probs, eval_values = player.network.run_many(positions)
+        mcts = strategies.MCTSPlayerMixin(
+            player.network,
+            simulations_per_move=FLAGS.readouts,
+            resign_threshold=-1)
 
-        for pos_name, probs, value in zip(pos_names, eval_probs, eval_values):
+        eval_trees = []
+        for name, position in eval_positions:
+            mcts.initialize_game(position)
+            mcts.suggest_move(position)
+
+            path = []
+            node = mcts.root
+            while node.children:
+                next_kid = np.argmax(node.child_N)
+                node = node.children.get(next_kid)
+                path.append("{},{}".format(node.fmove, int(node.N)))
+
             save_file = os.path.join(
-                FLAGS.data_dir, "heatmap-{}-{}.csv".format(pos_name, idx))
-
+                FLAGS.data_dir, "pv-{}-{}".format(name, idx))
             with open(save_file, "w") as data:
-                data.write("{},  {},  {}\n".format(
-                    idx, value, ",".join(map(str, probs))))
+                data.write("{},  {}\n".format(idx, ",".join(path)))
 
 
 def positions_from_sgfs(sgf_files):
-    positions = []
-    if include_empty:
-        # sgf_replay doesn't like SGFs with no moves played.
-        # Add the empty position for analysis manually.
-        positions.append(("empty", go.Position(komi=7.5)))
-
+    # sgf_replay doesn't like empty sgf file
+    data = [("empty", go.Position(komi=7.5))]
     for sgf in sgf_files:
         sgf_name = os.path.basename(sgf).replace(".sgf", "")
-        sgf_positions, moves, _ = oneoff_utils.parse_sgf(sgf)
-        final = sgf_positions[-1].play_move(moves[-1])
-        positions.append((sgf_name, final))
-    return positions
+        positions, moves, _ = oneoff_utils.parse_sgf(sgf)
+        final = positions[-1].play_move(moves[-1])
+        data.append((sgf_name, final))
+    return data
 
 
 def main(unusedargv):
     sgf_files = oneoff_utils.find_and_filter_sgf_files(FLAGS.sgf_dir)
     eval_positions = positions_from_sgfs(sgf_files)
 
-    eval_policy(eval_positions)
+    eval_pv(eval_positions)
 
 
 if __name__ == "__main__":
