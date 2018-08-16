a77ac5a9fae2a4fcff8ba18b2f389b6240ac3538
cclauss <cclauss@bluewin.ch>
2018-08-16 23:14:00 +0200

    Undefined name: import unittest in test_sgf_wrapper.py (#390)
    [flake8](http://flake8.pycqa.org) testing of https://github.com/tensorflow/minigo on Python 3.7.0
    
    $ __flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics__
    ```
    ./tests/test_sgf_wrapper.py:166:5: F821 undefined name 'unittest'
        unittest.main()
        ^
    1     F821 undefined name 'unittest'
    1
    ```

--- tests/test_sgf_wrapper.py
+++ tests/test_sgf_wrapper.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import unittest
 import go
 from sgf_wrapper import replay_sgf, translate_sgf_move, make_sgf
 
