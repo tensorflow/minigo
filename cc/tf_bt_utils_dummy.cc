cb14c7da037c97013a2cbf33a75c7da88c46a580
Derek T. Jones <dtj@google.com>
2018-10-14 11:38:35 -0700

    Add tfrzz_to_cbt game conversion tool
    Converts Minigo game examples from the `.tfrecord.zz`
    format in GCS, in batch, to a Bigtable target.
    Uses configurable multiprocessing to increase the rate
    and reset memory growth in the Bigtable C++ client.
    
    Can specify GCS file batches with `--glob_pattern`,
    and also specify input files as positional arguments.
    
    It converts synchronously by default, blocking the caller
    until all the games have been converted, then reports on
    rate statistics.  When aysynchronous, the tool forks
    and returns as soon as it has updated the game counter,
    which makes it friendly to any fleet orchestration:
    as soon as the tool exits, the next conversion can be
    undertaken on any machine, anywhere, since the game
    counter is atomically stored in the table itself.

--- cc/tf_bt_utils_dummy.cc
+++ cc/tf_bt_utils_dummy.cc
@@ -28,5 +28,23 @@ void WriteGameExamples(const std::string& gcp_project_name,
                 "Please recompile, passing --define=bt=1 to bazel build.";
 }
 
+uint64_t IncrementGameCounter(const std::string& gcp_project_name,
+                              const std::string& instance_name,
+                              const std::string& table_name, size_t delta) {
+  MG_FATAL() << "Can't increment a Bigtable game counter without Bigtable "
+                "support enabled. "
+                "Please recompile, passing --define=bt=1 to bazel build.";
+}
+
+void PortGamesToBigtable(const std::string& gcp_project_name,
+                         const std::string& instance_name,
+                         const std::string& table_name,
+                         const std::vector<std::string>& paths,
+                         int64_t game_counter) {
+  MG_FATAL() << "Can't port TFRecord ZLIB files to Bigtable without Bigtable "
+                "support enabled. "
+                "Please recompile, passing --define=bt=1 to bazel build.";
+}
+
 }  // namespace tf_utils
 }  // namespace minigo
